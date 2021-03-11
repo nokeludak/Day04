84786b87d11538b7ccc8efc151cac330f9e09ffa
nokeludak <ksmajic996@gmail.com>
2021-03-12 00:59:54 +0100

    Dodao ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+     var myFirstName = "Kenan";
+     var myLastName = "Smajić";
+     var myHomeTown = "Sarajevo";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
