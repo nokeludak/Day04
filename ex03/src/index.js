0a3debde94b019f5d82a8c115c3239009159ec20
nokeludak <ksmajic996@gmail.com>
2021-03-12 00:54:16 +0100

    Dodao ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,23 @@
+function main() {
+    var myRes = 16;
+    // Only change code below this line
+    myRes = myRes + 1;
+    // Only change code above this line
+    return myRes;
+}
+
+console.log(main());
+module.exports = main;
+
+/* 
+ var amina;
+ var amina=23;
+
+ amina--;
+ --amina;
+
+ i = i + 1;
+ i++;
+ i += 1;
+
+*/
